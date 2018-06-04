42be38e3c534a3c2d06632a868b17fcf7dbf5f7d
iroan <2654189525@qq.com>
2018-06-04 22:54:02 +0800

    add file

---
+++ vs/ConsoleApplication4/ex.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	using namespace std;
+	cout << "ÇëÊäÈëÄúµÄÃû×Ö" << endl;
+	char[32] name;
+	cin >> name;
+	cout << "name=" << name << endl;
+	system("pasue");
+}
