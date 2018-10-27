dc417b54ce0fbdccf5185e86f38edea01c013e1a
wangkaixuan <2654189525@qq.com>
2018-10-27 23:20:02 +0800

    失误的尝试-库和子目录源文件不一样

---
+++ cmake/test1/demo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "sub/demo_sub.cpp"
+using namespace std;
+int main(){
+	cout <<"wangkaixuan practice cmake demo!"<<endl;
+	myprintf();
+	return 0;
+}
---
+++ cmake/test1/sub/demo_sub.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+void myprintf(){
+cout<<"in sub dir"<<endl;
+}
